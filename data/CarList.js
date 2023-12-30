32cf38b7a12aa95b55ddf6ccae635dfdc5b3d956
philbulleyment <philbulleyment@yahoo.co.uk>
2023-12-30 15:23:29 +0000

    initial commit

---
+++ data/CarList.js
@@ -0,0 +1,165 @@
+const carList = [
+    {
+        name: "BMW M6 GT3 2017",
+        gtClass: "GT3"
+    },
+    {
+        name: "BMW M4 GT3 2021",
+        gtClass: "GT3"
+    },
+    {
+        name: "Emil Frey Jaguar GT3 2012",
+        gtClass: "GT3"
+    },
+    {
+        name: "Ferrari 488 GT3 2018",
+        gtClass: "GT3"
+    },
+    {
+        name: "Ferrari 488 EVO GT3 2020",
+        gtClass: "GT3"
+    },
+    {
+        name: "Ferrari 296 GT3 2023",
+        gtClass: "GT3"
+    },
+    {
+        name: "Honda NSX GT3 2017",
+        gtClass: "GT3"
+    },
+    {
+        name: "Honda NSX Evo GT3 2019",
+        gtClass: "GT3"
+    },
+    {
+        name: "Lamborghini Huracan GT3 2015",
+        gtClass: "GT3"
+    },
+    {
+        name: "Lamborghini Huracan Evo GT3 2019",
+        gtClass: "GT3"
+    },
+    {
+        name: "Lamborghini Huracan EVO2 GT3 2023",
+        gtClass: "GT3"
+    },
+    {
+        name: "Lexus RC F GT3 2016",
+        gtClass: "GT3"
+    },
+    {
+        name: "McLaren 650S GT3 2015",
+        gtClass: "GT3"
+    },
+    {
+        name: "McLaren 720S GT3 2019",
+        gtClass: "GT3"
+    },
+    {
+        name: "McLaren 720s Evo GT3 2023",
+        gtClass: "GT3"
+    },
+    {
+        name: "Mercedes AMG GT3 2015",
+        gtClass: "GT3"
+    },
+    {
+        name: "Mercedes AMG Evo GT3 2020",
+        gtClass: "GT3"
+    },
+    {
+        name: "Nissan GTR Nismo GT3 2015",
+        gtClass: "GT3"
+    },
+    {
+        name: "Nissan GTR Nismo GT3 2018",
+        gtClass: "GT3"
+    },
+    {
+        name: "Porsche 911 GT3 R 2018",
+        gtClass: "GT3"
+    },
+    {
+        name: "Porsche 911 II GT3R 2019",
+        gtClass: "GT3"
+    },
+    {
+        name: "Porsche 992 GT3R 2023",
+        gtClass: "GT3"
+    },
+    {
+        name: "Reiter Engineering R-EX GT3 2017",
+        gtClass: "GT3"
+    },
+    {
+        name: "Alpine A110 2018",
+        gtClass: "GT4"
+    },
+    {
+        name: "AMR V8 Vantage 2018",
+        gtClass: "GT4"
+    },
+    {
+        name: "Audi R8 LMS 2018",
+        gtClass: "GT4"
+    },
+    {
+        name: "BMW M4 2018",
+        gtClass: "GT4"
+    },
+    {
+        name: "Chevrolet Camaro R 2017",
+        gtClass: "GT4"
+    },
+    {
+        name: "Ginetta G55 2012",
+        gtClass: "GT4"
+    },
+    {
+        name: "KTM X-Bow 2016",
+        gtClass: "GT4"
+    },
+    {
+        name: "Maserati Granturismo MC 2016",
+        gtClass: "GT4"
+    },
+    {
+        name: "McLaren 570S 2016",
+        gtClass: "GT4"
+    },
+    {
+        name: "Mercedes AMG 2016",
+        gtClass: "GT4"
+    },
+    {
+        name: "Porsche 718 Cayman GT4 Clubsport 2019",
+        gtClass: "GT4"
+    },
+    {
+        name: "Ferrari 488 Challenge Evo 2020",
+        gtClass: "GTC"
+    },
+    {
+        name: "Lamborghini Huracan Super Trofeo 2015",
+        gtClass: "GTC"
+    },
+    {
+        name: "Lamborghini Huracan Super Trofeo Evo 2 2021",
+        gtClass: "GTC"
+    },
+    {
+        name: "Porsche 911 II GT3 Cup 2017",
+        gtClass: "GTC"
+    },
+    {
+        name: "Porsche 911 GT3 Cup (992) 2021",
+        gtClass: "GTC"
+    },
+    {
+        name: "BMW M2 CS 2020",
+        gtClass: "TCX"
+    },
+];
+
+
+module.exports = carList;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,63 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import carList from '../data/CarList';
+import { useState } from 'react';
 
 export default function Home() {
+  const [generatedCar, setGeneratedCar] = useState("");
+  const [carClass, setCarClass] = useState("all");
+
+  const handleGenerate = () => {
+    let randomCar = {};
+    if (carClass === 'all') {
+      randomCar = carList[Math.floor(Math.random() * carList.length)];
+    } else {
+      const filteredCarList = carList.filter(car => car.gtClass.toLowerCase() === carClass);
+      console.log(filteredCarList);
+      randomCar = filteredCarList[Math.floor(Math.random() * filteredCarList.length)];
+    }
+    
+    //display random car
+    setGeneratedCar(randomCar);
+  }
+
+  const handleSelectCarClass = (e) => {
+    setCarClass(e.target.value);
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Random ACC Car Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Random ACC Car Generator
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <select className='carClassSelectionBox' onChange={handleSelectCarClass}>
+          <option value="all">ALL</option>
+          <option value="gt3">GT 3</option>
+          <option value="gt4">GT 4</option>
+          <option value="gtc">GTC</option>
+          <option value="tcx">TCX</option>
+        </select>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <button onClick={handleGenerate} className='generateBtn'>Generate</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <h2 className='showCar'>
+          {generatedCar.name}
+        </h2>
+        <h3 className='showCarClass'>
+          {generatedCar.gtClass} </h3>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+
       </footer>
 
       <style jsx>{`
